a22e65960f218e5809ec5e92ea4ba58e28a1dbc1
Steven Pedersen <47424622+Invoke-STPE@users.noreply.github.com>
2022-11-22 13:10:54 +0100

    Created first test for lexer.
    Added TokenType, Lexer, IToken And TokenSymbol.

---
+++ RuddyRex.Lib/Lexer.cs
@@ -0,0 +1,17 @@
+﻿using RuddyRex.Lib.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RuddyRex.Lib
+{
+    public class Lexer
+    {
+        public static List<IToken> Tokenize(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RuddyRex.Tests/LexerTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RuddyRex.Lib;
+using RuddyRex.Lib.Enums;
+using RuddyRex.Lib.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RuddyRex.Tests
+{
+    [TestClass]
+    public class LexerTests
+    {
+        [TestMethod]
+        [DataRow("", "(", ")")]
+        public void Lexer_ShouldTokenizeSymbolPairs_ReturnTokenizedSymbols(string input, string open, string close)
+        {
+            List<IToken> expected = new() 
+            { 
+                new TokenSymbol() { Type = TokenType.Symbol, Value = open },
+                new TokenSymbol() { Type = TokenType.Symbol, Value = close },
+            };
+
+            List<IToken> actual = Lexer.Tokenize(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
